302e00e0a98010c9dafbaf1cdd4735cfa5ecb7e8
Christopher Goes <ghostofgoes@gmail.com>
2018-09-07 00:03:19 -0600

    Appveymoar

--- appveyor/run_tests.ps1
+++ appveyor/run_tests.ps1
@@ -8,17 +8,42 @@ echo $env:ci_type
 python --version
 python -c "import platform; print(platform.architecture())"
 
+echo "*** Beginning tests ***"
+
+echo "get-mac --help"
 get-mac --help
+
+echo "get-mac --version"
 get-mac --version
-python -m get-mac --help
-python -m get-mac --version
 
+echo "python -m getmac --help"
+python -m getmac --help
+
+echo "python -m getmac --version"
+python -m getmac --version
+
+echo "get-mac"
 get-mac
-python -m get-mac
-get-mac --debug
-python -m get-mac --debug
 
+echo "python -m getmac"
+python -m getmac
+
+echo "get-mac --debug"
+get-mac --debug
+
+echo "python -m getmac --debug"
+python -m getmac --debug
+
+echo "get-mac --debug -i 'Ethernet'"
 get-mac --debug -i 'Ethernet'
+
+echo "get-mac --debug -4 127.0.0.1"
 get-mac --debug -4 127.0.0.1
+
+echo "get-mac --debug --no-network-requests -4 127.0.0.1"
 get-mac --debug --no-network-requests -4 127.0.0.1
+
+echo "get-mac --debug -n localhost"
 get-mac --debug -n localhost
+
+echo "*** Tests completed ***"
