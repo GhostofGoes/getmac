f65a2624b9319b611d476a9337aca97957d4a4bf
Christopher Goes <ghostofgoes@gmail.com>
2018-09-13 19:35:53 -0600

    Make main much smaller

--- getmac/__main__.py
+++ getmac/__main__.py
@@ -1,50 +1,35 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import sys
-
 from . import getmac
 
 
 def main():
-    """CLI entrypoint. Invoked using `get-mac` or `python -m getmac`."""
-
     try:
         import argparse
     except ImportError:
-        print("You must install argparse on Python 2.6 and below. You can "
-              "install it with 'python -m pip install --user argparse'")
+        print("You must install argparse on Python 2.6 and below ('pip install argparse')")
         sys.exit(1)
 
-    parser = argparse.ArgumentParser(
-        description='Get the MAC address of remote hosts '
-                    'or network interfaces using Python.',
-        prog='get-mac', add_help=True)
-    parser.add_argument('--version', action='version',
-                        version='get-mac %s' % getmac.__version__)
-    parser.add_argument('--no-network-requests', action='store_true',
-                        help='Disable refreshing of the ARP '
-                             'table by making a network request (ping)')
+    parser = argparse.ArgumentParser('get-mac', description='Get the MAC address of system network interfaces or remote hosts on the LAN')
+    parser.add_argument('--version', action='version', version='get-mac %s' % getmac.__version__)
+    parser.add_argument('-d', '--debug', action='count', help='Enable debugging output. Add characters to increase verbosity of output, e.g. \'-dd\'.')
+    parser.add_argument('--no-network-requests', action='store_true', help='Do not send a UDP packet to refresh the ARP table')
+
     group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument('-i', '--interface', type=str, default=None,
-                       help='Name of a network interface on the system')
-    group.add_argument('-4', '--ip', type=str, default=None,
-                       help='IPv4 address of a remote host')
-    group.add_argument('-6', '--ip6', type=str, default=None,
-                       help='IPv6 address of a remote host')
-    group.add_argument('-n', '--hostname', type=str, default=None,
-                       help='Hostname of a remote host')
-    parser.add_argument('-d', '--debug', action='count',
-                        help='Enable increased output that is useful for '
-                             'debugging. Add more characters to increase '
-                             'the verbosity of output, e.g. \'-dd\'.')
+    group.add_argument('-i', '--interface', type=str, default=None, help='Name of a network interface on the system')
+    group.add_argument('-4', '--ip', type=str, default=None, help='IPv4 address of a remote host')
+    group.add_argument('-6', '--ip6', type=str, default=None, help='IPv6 address of a remote host')
+    group.add_argument('-n', '--hostname', type=str, default=None, help='Hostname of a remote host')
+
     args = parser.parse_args()
 
     if args.debug:
         getmac.DEBUG = args.debug
-    mac = getmac.get_mac_address(interface=args.interface, ip=args.ip,
-                                 ip6=args.ip6, hostname=args.hostname,
-                                 network_request=not args.no_network_requests)
+
+    mac = getmac.get_mac_address(
+        interface=args.interface, ip=args.ip,
+        ip6=args.ip6, hostname=args.hostname,
+        network_request=not args.no_network_requests)
 
     if mac is not None:
         print(mac)
