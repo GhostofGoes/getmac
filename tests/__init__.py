ecb2aa5c095ecd5cf5036ce5c7a8b10c22dfcafb
Izra Faturrahman <Frizz925@hotmail.com>
2018-10-03 03:05:33 +0700

    Remove load_sample method

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-from os import path
-
-def load_sample(filename):
-    filename = path.realpath('%s/../samples/%s' % (path.dirname(__file__), filename))
-    content = ''
-    with open(filename, 'r') as f:
-        content = f.read()
-    return content
