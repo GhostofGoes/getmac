7c0a3b7cb8ed48d26fe27947ef4199606c1f24b5
Christopher Goes <ghostofgoes@gmail.com>
2018-11-25 21:57:04 -0700

    Move RPM specs out of the project root

--- scripts/build-rpm-python2.sh
+++ scripts/build-rpm-python2.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SPECPATH="./packaging/python2-getmac.spec"
+
 sudo dnf install gcc rpm-build rpm-devel rpmlint make python bash coreutils diffutils patch rpmdevtools
 rpmdev-setuptree
 mkdir getmac-$1
@@ -9,7 +11,7 @@ cp -rt getmac-$1 getmac setup.py README.md LICENSE
 cp docs/man/getmac2.1 getmac-$1/docs/man
 tar czf getmac-$1.tar.gz getmac-$1
 mv getmac-$1.tar.gz ~/rpmbuild/SOURCES/
-cp python2-getmac.spec ~/rpmbuild/SPECS/
+cp "$SPECPATH" ~/rpmbuild/SPECS/
 rm -rf getmac-$1
 cd ~/rpmbuild/SPECS/
 rpmbuild -bs python2-getmac.spec
