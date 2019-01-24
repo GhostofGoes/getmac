d38709d3ebf0ac4c8911ef9054f1c0c46099e1d4
Christopher Goes <ghostofgoes@gmail.com>
2019-01-23 22:41:33 -0700

    Add unit tests for six more functions

--- tests/test_getmac.py
+++ tests/test_getmac.py
@@ -16,7 +16,8 @@ MAC_RE_DASH = r'([0-9a-fA-F]{2}(?:-[0-9a-fA-F]{2}){5})'
 def get_sample():
     def _get_sample(sample_path):
         sdir = path.realpath(path.join(path.dirname(__file__), '..', 'samples'))
-        with io.open(path.join(sdir, sample_path), 'rt', newline='', encoding='utf-8') as f:
+        with io.open(path.join(sdir, sample_path), 'rt',
+                     newline='', encoding='utf-8') as f:
             return f.read()
     return _get_sample
 
@@ -75,6 +76,7 @@ def test_call_proc(mocker):
 
 
 def test_windows_ctypes_host(mocker):
+    # mocker.patch.object(getmac, 'WINDOWS', True)
     pass
 
 
@@ -87,7 +89,10 @@ def test_uuid_ip(mocker):
 
 
 def test_uuid_lanscan_iface(mocker):
-    pass
+    mocker.patch('uuid._find_mac', return_value=2482700837424)
+    assert getmac._uuid_lanscan_iface('en1') == '02:42:0C:80:62:30'
+    mocker.patch('uuid._find_mac', return_value=None)
+    assert getmac._uuid_lanscan_iface('en0') is None
 
 
 def test_uuid_convert():
@@ -96,11 +101,19 @@ def test_uuid_convert():
 
 
 def test_read_sys_iface_file(mocker):
-    pass
+    mocker.patch('getmac.getmac._read_file', return_value='00:0c:29:b5:72:37\n')
+    assert getmac._read_sys_iface_file('ens33') == '00:0c:29:b5:72:37\n'
+    mocker.patch('getmac.getmac._read_file', return_value=None)
+    assert getmac._read_sys_iface_file('ens33') is None
 
 
-def test_read_arp_file(mocker):
-    pass
+def test_read_arp_file(mocker, get_sample):
+    data = get_sample('ubuntu_18.10/proc_net_arp.txt')
+    mocker.patch('getmac.getmac._read_file', return_value=data)
+    assert getmac._read_arp_file('192.168.16.2') == '00:50:56:e1:a8:4a'
+    assert getmac._read_arp_file('192.168.16.254') == '00:50:56:e8:32:3c'
+    assert getmac._read_arp_file('192.168.95.1') == '00:50:56:c0:00:0a'
+    assert getmac._read_arp_file('192.168.95.254') == '00:50:56:fa:b7:54'
 
 
 def test_read_file_return(mocker, get_sample):
@@ -119,21 +132,22 @@ def test_read_file_not_exist():
     assert getmac._read_file('DOESNOTEXIST') is None
 
 
-def test_hunt_for_mac(mocker):
-    pass
+def test_get_default_iface_linux(mocker, get_sample):
+    data = get_sample('ubuntu_18.10/proc_net_route.txt')
+    mocker.patch('getmac.getmac._read_file', return_value=data)
+    result = getmac._get_default_iface_linux()
+    assert result == 'ens33'
 
 
-def test_try_methods(mocker):
-    pass
-
-
-def test_get_default_iface_linux(mocker):
-    pass
-
-
-def test_hunt_linux_default_iface(mocker):
-    pass
+def test_hunt_linux_default_iface(mocker, get_sample):
+    data = get_sample('ubuntu_18.10/proc_net_route.txt')
+    mocker.patch('getmac.getmac._read_file', return_value=data)
+    result = getmac._hunt_linux_default_iface()
+    assert result == 'ens33'
 
 
 def test_fetch_ip_using_dns(mocker):
-    pass
+    m = mocker.patch('socket.socket')
+    m.return_value.getsockname.return_value = ('1.2.3.4', 51327)
+    result = getmac._fetch_ip_using_dns()
+    assert result == '1.2.3.4'
