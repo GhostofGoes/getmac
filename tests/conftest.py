e14609e22015c0c481af0f8b6e81b3dacf61c175
Christopher Goes <ghostofgoes@gmail.com>
2019-03-07 21:29:16 -0700

    Move samples into the tests directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ import pytest
 @pytest.fixture
 def get_sample():
     def _get_sample(sample_path):
-        sdir = path.realpath(path.join(path.dirname(__file__), '..', 'samples'))
+        sdir = path.realpath(path.join(path.dirname(__file__), 'samples'))
         with io.open(path.join(sdir, sample_path), 'rt',
                      newline='', encoding='utf-8') as f:
             return f.read()
