01e6c76894fa06a8b584714a7b1a45aba796e374
Christopher Goes <ghostofgoes@gmail.com>
2019-09-06 16:17:47 -0600

    Black format tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,10 @@ import pytest
 @pytest.fixture
 def get_sample():
     def _get_sample(sample_path):
-        sdir = path.realpath(path.join(path.dirname(__file__), 'samples'))
-        with io.open(path.join(sdir, sample_path), 'rt',
-                     newline='', encoding='utf-8') as f:
+        sdir = path.realpath(path.join(path.dirname(__file__), "samples"))
+        with io.open(
+            path.join(sdir, sample_path), "rt", newline="", encoding="utf-8"
+        ) as f:
             return f.read()
+
     return _get_sample
