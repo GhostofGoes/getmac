170fafef849f7ff014f8f9b8d7c1384552f06076
Christopher Goes <ghostofgoes@gmail.com>
2021-02-10 23:51:03 -0700

    rename rewrite => getmac

--- scripts/refactor_scratch.py
+++ scripts/refactor_scratch.py
@@ -1,31 +1,31 @@
 import logging
 import sys
-from getmac import rewrite
-rewrite.DEBUG = 2
+from getmac import getmac
+getmac.DEBUG = 2
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-rewrite.initialize_method_cache("iface")
-rewrite.initialize_method_cache("ip4")
-rewrite.initialize_method_cache("ip6")
-rewrite.initialize_method_cache("ip")
-rewrite.initialize_method_cache("default_iface")
+getmac.initialize_method_cache("iface")
+getmac.initialize_method_cache("ip4")
+getmac.initialize_method_cache("ip6")
+getmac.initialize_method_cache("ip")
+getmac.initialize_method_cache("default_iface")
 
-print(rewrite.METHOD_CACHE)
-print(rewrite.FALLBACK_CACHE)
+print(getmac.METHOD_CACHE)
+print(getmac.FALLBACK_CACHE)
 
-print(rewrite.get_by_method("ip4", "10.0.0.1"))
-print(rewrite.get_by_method("ip4", "10.0.0.20"))
-print(rewrite.get_by_method("ip4", "10.0.0.33"))
-print(rewrite.get_by_method("iface", "Ethernet 4"))
-print(rewrite.get_by_method("iface", "eth0"))
-print(rewrite.get_by_method("iface", "enp1s0"))
-print(rewrite.get_by_method("default_iface"))
+print(getmac.get_by_method("ip4", "10.0.0.1"))
+print(getmac.get_by_method("ip4", "10.0.0.20"))
+print(getmac.get_by_method("ip4", "10.0.0.33"))
+print(getmac.get_by_method("iface", "Ethernet 4"))
+print(getmac.get_by_method("iface", "eth0"))
+print(getmac.get_by_method("iface", "enp1s0"))
+print(getmac.get_by_method("default_iface"))
 
-print(rewrite.get_mac_address(ip="10.0.0.1"))
-print(rewrite.get_mac_address(ip="10.0.0.20"))
-print(rewrite.get_mac_address(ip="10.0.0.33"))
-print(rewrite.get_mac_address(interface="Ethernet 4"))
-print(rewrite.get_mac_address(interface="eth0"))
-print(rewrite.get_mac_address(interface="enp1s0"))
-print(rewrite.get_mac_address())
+print(getmac.get_mac_address(ip="10.0.0.1"))
+print(getmac.get_mac_address(ip="10.0.0.20"))
+print(getmac.get_mac_address(ip="10.0.0.33"))
+print(getmac.get_mac_address(interface="Ethernet 4"))
+print(getmac.get_mac_address(interface="eth0"))
+print(getmac.get_mac_address(interface="enp1s0"))
+print(getmac.get_mac_address())
