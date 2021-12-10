c5b9ce0acf21cd327046d21638f905d9b8430801
Christopher Goes <ghostofgoes@gmail.com>
2021-12-10 13:10:04 -0700

    Deprecate Python 3.4 and 3.5, emit as warning

--- getmac/getmac.py
+++ getmac/getmac.py
@@ -32,6 +32,7 @@ import socket
 import struct
 import sys
 import traceback
+import warnings
 from subprocess import check_output
 
 try:  # Python 3
@@ -103,8 +104,8 @@ try:
 except ImportError:
     pass
 
-# Ensure we only log the Python 2 warning once
-WARNED_PY2 = False
+# Ensure we only log the Python compatibility warning once
+WARNED_UNSUPPORTED_PYTHONS = False
 
 
 def get_mac_address(
@@ -135,14 +136,18 @@ def get_mac_address(
         Lowercase colon-separated MAC address, or None if one could not be
         found or there was an error.
     """
-    if PY2:
-        global WARNED_PY2
-        if not WARNED_PY2:
-            log.warning(
-                "Python 2 compatibility will be dropped in getmac 1.0.0. If you are "
-                'stuck on Python 2, consider loosely pinning the version e.g. "getmac<1".'
+    if PY2 or (sys.version_info[0] == 3 and sys.version_info[1] < 6):
+        global WARNED_UNSUPPORTED_PYTHONS
+        if not WARNED_UNSUPPORTED_PYTHONS:
+            warning_string = (
+                "Support for Python versions < 3.6 is deprecated and will be "
+                "removed in getmac 1.0.0. If you are stuck on an unsupported "
+                "Python, considor loosely pinning the version of this package "
+                'in your dependency list, e.g. "getmac<1".'
             )
-            WARNED_PY2 = True
+            warnings.warn(warning_string, DeprecationWarning)
+            log.warning(warning_string)  # Ensure it appears in any logs
+            WARNED_UNSUPPORTED_PYTHONS = True
 
     if (hostname and hostname == "localhost") or (ip and ip == "127.0.0.1"):
         return "00:00:00:00:00:00"
