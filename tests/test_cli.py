349ecfdc52f9b973ac94f8856ce244f9407b08b8
Christopher Goes <ghostofgoes@gmail.com>
2023-06-02 22:40:00 -0600

    some test improvements

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,16 +1,16 @@
 import platform
 import sys
 from subprocess import PIPE, Popen
+from typing import List
 
 from getmac import __version__, get_mac_address
 
 BASE_CMD = [sys.executable, "-m", "getmac"]
 
 
-def run_cmd(command):
-    # type: (list) -> str
-    stdout, _ = Popen(command, stdout=PIPE, stderr=PIPE).communicate()
-    return stdout.decode("utf-8").strip()
+def run_cmd(command: List[str]) -> str:
+    stdout, stderr = Popen(command, stdout=PIPE, stderr=PIPE).communicate()
+    return stdout.decode("utf-8").strip() + stderr.decode().strip()
 
 
 def test_cli_main_basic():
@@ -30,11 +30,11 @@ def test_cli_main_invalid_interface():
 
 
 def test_cli_help():
-    assert run_cmd([*BASE_CMD, "--help"]) != ""
+    assert "usage: getmac" in run_cmd([*BASE_CMD, "--help"])
 
 
 def test_cli_version():
-    assert __version__ in run_cmd([*BASE_CMD, "--version"])
+    assert run_cmd([*BASE_CMD, "--version"]).strip().endswith(__version__)
 
 
 def test_cli_multiple_debug_levels():
@@ -52,18 +52,23 @@ def test_cli_no_net():
 def test_cli_override_port():
     assert (
         run_cmd([*BASE_CMD, "-v", "-dd", "-4", "127.0.0.1", "--override-port", "44444"])
-        != ""
     )
 
 
 def test_cli_localhost():
-    assert run_cmd([*BASE_CMD, "-4", "127.0.0.1"]) != ""
-    assert run_cmd([*BASE_CMD, "-n", "localhost"]) != ""
-    assert run_cmd([*BASE_CMD, "--no-network-requests", "-4", "127.0.0.1"]) != ""
-    assert run_cmd([*BASE_CMD, "--no-network-requests", "-n", "localhost"]) != ""
+    assert run_cmd([*BASE_CMD, "-4", "127.0.0.1"])
+    assert run_cmd([*BASE_CMD, "-n", "localhost"])
+    assert run_cmd([*BASE_CMD, "--no-network-requests", "-4", "127.0.0.1"])
+    assert run_cmd([*BASE_CMD, "--no-network-requests", "-n", "localhost"])
 
 
 # TODO: figure out how to properly test CLI commands and isolate platform-specific behavior
 def test_cli_override_platform():
+    # TODO: proper test for this
     plat = platform.system().lower()
-    assert run_cmd([*BASE_CMD, "-v", "-dd", "--override-platform", plat]) != ""
+    assert run_cmd([*BASE_CMD, "-v", "-dd", "--override-platform", plat])
+
+
+def test_cli_force_method():
+    # TODO: proper test for this
+    assert run_cmd([*BASE_CMD, "-v", "-dd", "--force-method", "InvalidMethod"])
